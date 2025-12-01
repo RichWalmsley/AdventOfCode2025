038d6a67a721b56fe5eed2c4147d5e4523770043
rwalmsley <rwalmsley@krakenrobotics.com>
2025-12-01 11:46:01 -0330

    Finished day 1 part 1. Started day 1 part 2.

---
+++ Day1/Day1Part1.hs
@@ -0,0 +1,63 @@
+import System.IO
+import Data.Maybe (mapMaybe)
+import Text.Read (readMaybe)
+
+-- Data types
+data Direction
+    = L
+    | R
+    deriving (Read, Show, Eq)
+
+data Instruction = Instruction
+    { direction :: Direction
+    , units     :: Int
+    } deriving (Show, Eq) 
+
+getDirection :: Char -> Maybe Direction
+getDirection c =
+    case c of
+        'L' -> Just L
+        'R' -> Just R
+        _ -> Nothing
+
+extractInstruction :: String -> Maybe Instruction 
+extractInstruction [] = Nothing
+extractInstruction (dir : units) = do
+    d <- getDirection dir
+    u <- readMaybe units
+    return (Instruction d u)
+
+constructInstructionList :: [String] -> [Instruction]
+constructInstructionList = mapMaybe extractInstruction
+
+turnDial
+    :: Int -- initial dial pos
+    -> Instruction
+    -> Int -- final dial pos
+turnDial pos (Instruction L u) = wrap(pos - u)
+turnDial pos (Instruction R u) = wrap(pos + u)
+
+wrap :: Int -> Int
+wrap x = (x `mod` 100 + 100) `mod` 100
+
+executeInstructions
+    :: [Instruction] -- instruction list
+    -> Int -- dial position
+    -> Int -- accumulator for 0's
+    -> Int -- final output of 0's counted
+executeInstructions [] _ acc = acc
+executeInstructions (instr : rest) pos acc =
+    case pos' of
+        0 -> executeInstructions rest pos' (acc + 1)
+        _ -> executeInstructions rest pos' acc
+    where
+        pos' = turnDial pos instr
+
+-- Main
+main :: IO ()
+main = do
+    contents <- readFile "input"
+    let instructions = constructInstructionList $ lines contents
+    let zeroCount = executeInstructions instructions 50 0
+    putStrLn contents
+    print zeroCount
